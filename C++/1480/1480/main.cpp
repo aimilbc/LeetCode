a6d3ea1c6ae074dcfd8ddd80af6950dd3dec4bd6
aimilbc <touyamaaimi927@gmail.com>
2020-06-17 11:56:39 -0700

    updates
    updating c++ files

---
+++ C++/1480/1480/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1480
+//
+//  Created by Aimi Ross on 6/17/20.
+//  Copyright © 2020 Aimi Ross. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
